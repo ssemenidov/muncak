84348b0ba781b5078102b9d27305d43807ed063c
ssemenidov <23sergey03@gmail.com>
2020-05-29 21:08:19 +0800

    anim overlay

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ for (let i = 0; i < footerItemList.length; i++) {
     footerContentList[i].classList.toggle("footer-item-content--active");
   });
 }
+let sub_btn = document.getElementById("sub_btn");
+let overlay = document.getElementById("overlay");
+sub_btn.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+  setTimeout(function () {
+    overlay.classList.remove("overlay--active");
+  }, 3000);
+});
